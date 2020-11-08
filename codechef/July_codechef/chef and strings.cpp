eaa02af56bb0b74e393f0cbebe76dfe260c1b19c
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-08 19:07:58 +0530

    Update chef and strings.cpp

--- codechef/July_codechef/chef and strings.cpp
+++ codechef/July_codechef/chef and strings.cpp
@@ -1,3 +1,25 @@
+/**
+Having already mastered cooking, Chef has now decided to learn how to play the guitar. Often while trying to play a song, Chef has to skip several strings to reach the string he has to pluck. Eg. he may have to pluck the 1st string and then the 6th string. This is easy in guitars with only 6 strings; However, Chef is playing a guitar with 106 strings. In order to simplify his task, Chef wants you to write a program that will tell him the total number of strings he has to skip while playing his favourite song.
+This is how guitar strings are numbered (In ascending order from right to left). Eg. to switch from string 1 to 6, Chef would have to skip 4 strings (2,3,4,5).
+Sample Input:
+2
+6
+1 6 11 6 10 11
+4
+1 3 5 7
+Sample Output:
+15
+3
+Explanation:
+Test Case 1
+
+Chef skips 4 strings (2,3,4,5) to move from 1 to 6
+Chef skips 4 strings (7,8,9,10) to move from 6 to 11
+Chef skips 4 strings (10,9,8,7) to move from 11 to 6
+Chef skips 3 strings (7,8,9) to move from 6 to 10
+Chef skips 0 strings to move from 10 to 11
+Therefore, the answer is 4+4+4+3+0=15
+*/
 #include <iostream>
 using namespace std;
 
