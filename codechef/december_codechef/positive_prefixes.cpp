7416191ecf52ed49e5c57ab89ef47960c82f1690
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-12-11 20:19:25 +0530

    added positive prefixes

---
+++ codechef/december_codechef/positive_prefixes.cpp
@@ -0,0 +1,73 @@
+/**
+You are given two positive integers N and K, where K≤N. Find a sequence A1,A2,…,AN such that:
+
+for each valid i, Ai is either i or −i
+there are exactly K values of i such that 1≤i≤N and A1+A2+…+Ai>0
+If there are multiple solutions, you may print any one of them. It can be proved that at least one solution always exists.
+
+Input
+The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+The first and only line of each test case contains two space-separated integers N and K.
+Output
+For each test case, print a single line containing N space-separated integers A1,A2,…,AN.
+
+Constraints
+1≤T≤1,000
+1≤K≤N≤1,000
+Subtasks
+Subtask #1 (10 points): N≤10
+Subtask #2 (90 points): original constraints
+
+Example Input
+1
+3 3
+Example Output
+1 2 3
+*/
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int n,k,count=0;
+	    cin>>n>>k;
+	    vector<long long int>v;
+	    for(int i=0;i<n;i++)
+	    v.push_back(i+1);
+	    if(n==k)
+	    {
+	        for(int i=0;i<n;i++)
+	        cout<<v[i]<<" ";
+	        cout<<"\n";
+	    }
+	    else
+	    {
+	    for(int i=0;i<n;i+=2)
+	    v[i]=-1*v[i];
+	    for(int i=0;i<n;i++)
+	    {
+	        if(v[i]<0)
+	        count++;
+	    }
+	    for(int i=n-1;i>=0;i--)
+	    {
+	        if(v[i]>0)
+	        {
+	            count++;
+	            v[i]=-1*v[i];
+	        }
+	        if(count==n-k)
+	        break;
+	    }
+	    for(int i=0;i<n;i++)
+	    cout<<v[i]<<" ";
+	    cout<<"\n";
+	    }
+	    
+	}
+	return 0;
+}
