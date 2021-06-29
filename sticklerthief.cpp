31075ef334f1d0188c7e14704789584981cb4fc3
Ojaswi Awasthi <ojaswiawasthi2025@gmail.com>
2021-06-30 01:44:09 +0530

    Create sticklerthief.cpp

---
+++ sticklerthief.cpp
@@ -0,0 +1,19 @@
+
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        int dp[n];
+        if(n==0)
+        return 0;
+        if(n==1)
+        return arr[0];
+        dp[0]=arr[0];
+        dp[1]=max(arr[0],arr[1]);
+        for(int i=2;i<n;i++)
+        dp[i]=max(dp[i-1],dp[i-2]+arr[i]);
+        return dp[n-1];
+    }
+};
